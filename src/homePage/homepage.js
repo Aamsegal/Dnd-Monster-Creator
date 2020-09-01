d0a93a1936da48b3355ca8ede3bf804931665257
Aaron <amsegal93@gmail.com>
2020-09-01 14:36:10 -0400

    Added homescreen css. More info button the pops up a little blurb. Css is only for mobile so far

--- src/homePage/homepage.js
+++ src/homePage/homepage.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import './homePage.css'
+import './homepage.css'
 
 class Homepage extends Component {
+
+    moreInfoButton() {
+        document.getElementById('moreInfoPopup').style.display = "block";
+    }
+
+    closeMoreInfo() {
+        document.getElementById('moreInfoPopup').style.display = "none"
+    }
 
     render() {
 
         return (
-            <main>
-                <h1>Dnd Monster Creator</h1>
-                <p>You can create a dnd monster with this App.</p>
-                <p>Adjust stats, add attacks, notes and more.</p>
-                <p>Either sign in with your username to save monsters or just start creating a monster.</p>
-                <button><Link to='/application'>Continue</Link></button>
-            </main>
+            <header className = "introductionContainer">
+
+                <div className = "introduction" id = "introduction">
+                    <h1>Dnd Monster Creator</h1>
+                    <h3>Create, Save, Enjoy!</h3>
+                    <button><Link to='/application'>Get Started</Link></button>
+                    <button onClick={this.moreInfoButton}>More Info</button>
+                </div>
+
+                <div className = "moreInfoPopup" id="moreInfoPopup">
+                    <div className = "moreInfoPopup-Content" id="moreInfoPopup-Content">
+                        <p>This tool will allow you to build your own D&d. You can log in to save your moster, 
+                        or just take a screen shot when you are done. If you are stuck, feel free to choose a
+                        combat raiting from the suggestion bar to get a starting point for your creature. Enjoy!</p>
+                       
+                        <div className = "close-Button-div" id = "close-Button-div">
+                            <button onClick={this.closeMoreInfo}>Close</button>
+                        </div>
+                        
+                    </div>
+                </div>
+                    
+            </header>
             
         )
     }
