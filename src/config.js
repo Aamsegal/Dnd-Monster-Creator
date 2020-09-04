4e03eef792cf642387bd3fab31b84f31836ff7ee
Aaron <amsegal93@gmail.com>
2020-09-04 15:36:00 -0400

    Forgot to save the file pointing to the server endpoint. fixed it to look for the web hosted server instead of local host:

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    //API_ENDPOINT: 'https://dnd-monster-creator-server.herokuapp.com'
-    API_ENDPOINT: 'https://localhost:8000'
+    API_ENDPOINT: 'https://dnd-monster-creator-server.herokuapp.com'
+    //API_ENDPOINT: 'https://localhost:8000'
   }
   
